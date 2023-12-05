5f5c98b9b002eb30074260538c5cab51ad4fe949
sahanapagati <sahanapagati@gmail.com~>
2023-12-05 17:31:29 +0530

    changed

--- src/main/java/com/example/schedular/SchedularApplication.java
+++ src/main/java/com/example/schedular/SchedularApplication.java
@@ -2,9 +2,16 @@ package com.example.schedular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SchedularApplication {
+public class SchedularApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SchedularApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedularApplication.class, args);
